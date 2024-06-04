21bd2a51496b147740ad4b616848381f5b5fafef
smmstakes <matheusds014@gmail.com>
2024-06-04 10:38:24 -0300

    Blink: modificando para deixar o LED somente desligado

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,23 +1,9 @@
-////////////////////////////////////////////////////////////////////////////////
-// Automotive Network Evaluation Board v1.0 (Arquivo Blink.h)
-// Pisca um led a cada 1 segundo
-// Exemplo de domínio público
-// https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-////////////////////////////////////////////////////////////////////////////////
 #include "board.h"
-// Executa somente uma vez após pressionar o botão reset ou ligar
+
 void setup() {
-// Inicializa o pino digital como entrada.
-pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 //Executa continuamente
 void loop() {
-//Liga o led
-//digitalWrite (LED_BUILTIN, HIGH);
-//Aguarda 500ms
-//delay(500);
-//Desliga o led
-digitalWrite (LED_BUILTIN, LOW);
-//Aguarda 500ms
-//delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
 }
